6ad37730556d251e3a01b124d7e70599f29fc7c8
nikhilak7 <knikhila752@gmail.com>
2020-07-05 12:53:39 +0530

    Basic Assertion in writing the tests with Cypress

--- cypress/integration/examples/testOne.js
+++ cypress/integration/examples/testOne.js
@@ -1,5 +1,11 @@
+/// <reference types="Cypress" />
+
 describe('My First Test Suite', function() {
     it('My FirstTest case', function() {
       cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+      cy.get('.search-keyword').type('ca');
+      cy.wait(2000);
+      cy.get('.product').should('have.length', 4);
+
     });
   });
