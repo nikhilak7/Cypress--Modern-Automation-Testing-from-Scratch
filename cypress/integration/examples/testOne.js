8a8e40a886c4fa36e14c8b626d55276f6e5c15c0
nikhilak7 <knikhila752@gmail.com>
2020-07-05 13:30:07 +0530

    Handling Invisible elements with Cypress by understanding logs

--- cypress/integration/examples/testOne.js
+++ cypress/integration/examples/testOne.js
@@ -5,7 +5,7 @@ describe('My First Test Suite', function() {
       cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
       cy.get('.search-keyword').type('ca');
       cy.wait(2000);
-      cy.get('.product').should('have.length', 4);
+      cy.get('.product:visible').should('have.length', 4);
 
     });
   });
