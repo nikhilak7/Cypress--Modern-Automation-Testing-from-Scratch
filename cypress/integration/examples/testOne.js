685e6d6d3755ca67e91d8365a12f17eea27b2651
nikhilak7 <knikhila752@gmail.com>
2020-07-06 21:02:15 +0530

    Understanding get and find commands with cypress

--- cypress/integration/examples/testOne.js
+++ cypress/integration/examples/testOne.js
@@ -1,11 +1,16 @@
 /// <reference types="Cypress" />
 
-describe('My First Test Suite', function() {
-    it('My FirstTest case', function() {
-      cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-      cy.get('.search-keyword').type('ca');
-      cy.wait(2000);
-      cy.get('.product:visible').should('have.length', 4);
+describe("My First Test Suite", function () {
+  it("My FirstTest case", function () {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.get(".search-keyword").type("ca");
+    cy.wait(2000);
 
-    });
+    cy.get(".product").should("have.length", 5);
+    cy.get(".product:visible").should("have.length", 4);
+    //Parent child chaining
+    cy.get(".products").find(".product").should("have.length", 4);
+
+    cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
   });
+});
