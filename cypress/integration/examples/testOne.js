8442bc727117616ef48a1dd2415be1f33a274b71
nikhilak7 <knikhila752@gmail.com>
2020-07-17 16:55:22 +0530

    Grabbing the text for validations using text command

--- cypress/integration/examples/testOne.js
+++ cypress/integration/examples/testOne.js
@@ -12,5 +12,14 @@ describe("My First Test Suite", function () {
     cy.get(".products").find(".product").should("have.length", 4);
 
     cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
+
+    cy.get(".products").find(".product").each(($el, index, $list) => {
+       
+    const textVeg = $el.find('h4.product-name').text();
+    if(textVeg.includes('Cashews'))
+    {
+    $el.find('button').click();
+    }
+});
   });
 });
