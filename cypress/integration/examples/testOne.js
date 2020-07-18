3eb052d03b0361c71655896e87a461bc8919f105
nikhilak7 <knikhila752@gmail.com>
2020-07-18 21:52:39 +0530

    Handling Async promises

--- cypress/integration/examples/testOne.js
+++ cypress/integration/examples/testOne.js
@@ -5,26 +5,41 @@ describe("My First Test Suite", function () {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get(".search-keyword").type("ca");
     cy.wait(2000);
-
+    
     cy.get(".product").should("have.length", 5);
     cy.get(".product:visible").should("have.length", 4);
     //Parent child chaining
-    cy.get(".products").find(".product").should("have.length", 4);
+    cy.get(".products").as("productLocator");
+    cy.get("@productLocator").find(".product").should("have.length", 4);
+    cy.get(":nth-child(3) > .product-action > button").click();
+    cy.get("@productLocator")
+      .find(".product")
+      .eq(2)
+      .contains("ADD TO CART")
+      .click()
+      .then(function () {
+        console.log("sf");
+      });
 
-    cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
+    cy.get("@productLocator")
+      .find(".product")
+      .each(($el, index, $list) => {
+        const textVeg = $el.find("h4.product-name").text();
+        if (textVeg.includes("Cashews")) {
+          $el.find("button").click();
+        }
+      });
 
-    cy.get(".products").find(".product").each(($el, index, $list) => {
-       
-    const textVeg = $el.find('h4.product-name').text();
-    if(textVeg.includes('Cashews'))
-    {
-    $el.find('button').click();
-    }
-});
-cy.get('.brand').then(function(logoelement) {
-    cy.log(logoelement.text());
-});
+    cy.get(".brand").should("have.text", "GREENKART");
 
+    //this is to print in logs
+    cy.get(".brand").then(function (logoelement) {
+      cy.log(logoelement.text());
+    });
+    //const logo=cy.get('.brand')
+    //cy.log(cy.get('.brand').text())
+    // cy.log(logo.text())
 
+    //fixture
   });
 });
